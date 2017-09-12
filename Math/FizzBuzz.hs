27168928104dc0b9a217d9169695212a3193c623
Mike J Hewitt <mikeh@electroteach.com>
2017-09-12 22:01:25 +1000

    after hindent

--- Math/FizzBuzz.hs
+++ Math/FizzBuzz.hs
@@ -1,16 +1,17 @@
-module Math.FizzBuzz (
-toFizz
-) where 
+module Math.FizzBuzz
+  ( toFizz
+  ) where
 
 import Control.Monad.Instances
 
 toFizz :: Int -> String
 toFizz = do
-	f <- (==0) . (flip mod 3)
-	b <- (==0) . (flip mod 5)
-	p <- show
-	return $ case [f,b] of
-		[True,True] -> "fizzbuzz"
-		[True,False] -> "fizz"
-		[False,True] -> "buzz"
-		[False,False] -> p
+  f <- (== 0) . (flip mod 3)
+  b <- (== 0) . (flip mod 5)
+  p <- show
+  return $
+    case [f, b] of
+      [True, True] -> "fizzbuzz"
+      [True, False] -> "fizz"
+      [False, True] -> "buzz"
+      [False, False] -> p
